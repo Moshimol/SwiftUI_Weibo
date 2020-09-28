d584a98d2c43406f773534771f8066f4dfe08228
lushitong <269797304@qq.com>
2020-09-28 20:11:31 +0800

    添加单独的Cel

--- SwiftUIX/ContentView.swift
+++ SwiftUIX/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
-        Text("312").padding()
     }
 }
 
+// 自动预览的页面
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
