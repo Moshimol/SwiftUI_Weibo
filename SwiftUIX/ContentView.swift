dffc117f9592c8f3b6242cf0fa5cc96954a50c1d
lushitong <269797304@qq.com>
2020-09-28 19:14:52 +0800

    Initial Commit

---
+++ SwiftUIX/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIX
+//
+//  Created by lushitong on 2020/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+        Text("312").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
