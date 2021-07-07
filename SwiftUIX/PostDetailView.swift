3c822ee9a16352277086947d804421309782dde0
lushitong <269797304@qq.com>
2021-07-07 23:37:14 +0800

    feat:微博详情页面

---
+++ SwiftUIX/PostDetailView.swift
@@ -0,0 +1,29 @@
+//
+//  PostDetailView.swift
+//  SwiftUIX
+//
+//  Created by lushitong on 2021/7/7.
+//
+
+import SwiftUI
+
+struct PostDetailView: View {
+    let post:Post
+    var body: some View {
+        List {
+            PostCell(post: post)
+                .listRowInsets(EdgeInsets())
+            
+            ForEach(1...200, id:\.self) { i in
+                Text("评论\(i)")
+            }
+        }
+        .navigationBarTitle("详情",displayMode: .inline)
+    }
+}
+
+struct PostDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostDetailView(post: postList.list[0])
+    }
+}
